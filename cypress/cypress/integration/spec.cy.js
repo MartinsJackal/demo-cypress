b2672d6992bf7fe5ded8f4e69cab2afb0abbdfd1
João Victor Martins <63604234+MartinsJackal@users.noreply.github.com>
2024-10-20 18:43:00 -0300

    Update spec.cy.js

--- cypress/cypress/integration/spec.cy.js
+++ cypress/cypress/integration/spec.cy.js
@@ -1,31 +1,30 @@
 describe('Teste End-to-End', () => {
-    it('Teste 1: Visita Página', () => {
+    
+    beforeEach(() => {
         cy.visit('http://localhost:5000/');
     });
 
-    it('Teste 2: Verifica item na página', () => {
+    it('Teste 1: Verifica item na página', () => {
         cy.get('[data-id=3]').should('contain.text', 'Design Patterns');
-    });    
+    });
 
-    it('Teste 3: Calcula Frete', () => {
+    it('Teste 2: Calcula Frete', () => {
         cy.get('[data-id=3]').within(() => {
             cy.get('input').type('10000-000');
             cy.contains('Calcular Frete').click();
-            cy.wait(2000);
         });
-        cy.get('.swal-text').contains('O frete é: R$');
+
+        cy.get('.swal-text').should('contain.text', 'O frete é: R$');
         cy.get('.swal-button').click();
     });
 
-    it('Teste 4: Realizar compra do livro', () => {
-        // Seleciona e clica no botão Comprar
+    it('Teste 3: Realizar compra do livro', () => {
+        // Clica no botão "Comprar"
         cy.contains('Comprar').click();
 
-        // Espera que o pop-up apareça
-        cy.wait(2000); // Ajuste o tempo de espera conforme necessário
-
-        // Verifica a mensagem de confirmação
-        cy.get('.swal-text').should('contain.text', 'Sua compra foi realizada com sucesso');
+        // Aguarda o pop-up de sucesso usando espera dinâmica
+        cy.get('.swal-text', { timeout: 5000 })
+            .should('contain.text', 'Sua compra foi realizada com sucesso');
 
         // Fecha o pop-up clicando no botão
         cy.get('.swal-button').click();
